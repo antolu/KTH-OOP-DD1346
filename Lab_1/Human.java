4b58f7d63cbc3bf89d7e8759611b1ebe938c78b5
Anton Lu <anton.lu@epfl.ch>
2018-06-09 23:38:21 +0200

    E3

--- Lab_1/Human.java
+++ Lab_1/Human.java
@@ -5,6 +5,30 @@ public class Human {
     private int age;
     private String name;
 
+    private String names[] = {
+        "Anton", "Elisabet", "Felicitas", "Gisele", "Chanell", "Coralie", 
+        "Raymundo", "Deshawn", "Maryellen", "Alden", "Tena", "Hosea", "Nila", 
+        "King", "Adolph", "Phillis", "Treasa", "Jan", "Libbie", "Kristina", "Silva", 
+        "Windy", "Lizette", "Zackary", "Evonne", "Ethyl", "Cierra", "Luis", "Roselee", 
+        "Maryann", "Molly", "Tinisha", "Chante", "Emmie", "Byron", "Jacque", "Chantelle", 
+        "Laura", "Cindi", "Valeri", "Melba", "Alise", "Sanjuana", "Mervin", "Eugena", 
+        "Weldon", "Salena", "Marquita", "Dottie", "Nanci", "Trinh", "Tashia", "Maria", 
+        "Bailey", "Stanford", "Zonia", "Rose", "Josue", "Jim", "Alaina", "Ambrose", 
+        "Merilyn", "Fredrick", "Yesenia", "Jaclyn", "Corrie", "Devora", "Tiffany", 
+        "Timothy", "Kristel", "Lecia", "Julene", "Shera", "Crissy", "Vernie", "Willia", 
+        "Moises", "Krystyna", "Gwendolyn", "Tomi", "Katy", "Raeann", "Carolina", "Charita", 
+        "Alpha", "Margorie", "Kaylee", "Federico", "Callie", "Laurence", "Dedra", "Jody", 
+        "Ivana", "Shizue", "Dawna", "Freddie", "Arthur", "Joel", "Isabell", "Antoine", 
+        "Annetta", "Edna"
+    };
+    public Human() // Random
+    {
+        double randNo = 100*Math.random();
+        int index = (int)Math.floor(randNo);
+        name = names[index];
+        age = index;
+    }
+
     public Human(int ageIn, String nameIn)
     {
         age = ageIn;
--- Lab_1/Main.java
+++ Lab_1/Main.java
@@ -1,7 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         Human Pelle = new Human(25, "Pelle");;
-        System.out.println(Pelle.toString());
+        // System.out.println(Pelle.toString());
+        System.out.println("E1-E2");
         System.out.println(Pelle);
+
+        System.out.println("");
+
+        /*******************************/
+
+        System.out.println("E3");
+
+        int numberOfHumans = 10;
+
+        Human Humans[] = new Human[numberOfHumans];
+
+        for (int i = 0; i < numberOfHumans; i++)
+        {
+            Humans[i] = new Human();
+        }
+
+        for (int i = 0; i < numberOfHumans; i++)
+        {
+            System.out.println(Humans[i]);
+        }
     }
 }

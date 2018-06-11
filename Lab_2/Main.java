846c4b9f69e703092e71aa7af40eba1a16557f19
Anton Lu <anton.lu@epfl.ch>
2018-06-11 21:52:05 +0200

    L2_E2

---
+++ Lab_2/Main.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;  
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        JFrame frame = new JFrame("A button");
+        MyButton button = new MyButton();
+        JPanel panel = new JPanel();
+
+
+        frame.getContentPane().setBackground(Color.white);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Closes window when (x) is pressed in OS
+        // frame.setPreferredSize(new Dimension(500,500));
+
+        // button.setPreferredSize(new Dimension(200,100));
+
+        // panel.setPreferredSize(new Dimension(300, 150));
+
+        panel.add(button);
+        frame.add(panel);
+        frame.pack(); // Size frame to match contents
+
+        button.setVisible(true);
+        panel.setVisible(true);
+        frame.setVisible(true);
+    }
+}

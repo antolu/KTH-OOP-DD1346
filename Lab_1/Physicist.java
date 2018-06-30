8637105d5acd72b0b4c3240f9623c074e561b1fa
Anton Lu <anton.lu@epfl.ch>
2018-06-30 17:48:16 +0200

    L1 C1 String formatting

--- Lab_1/Physicist.java
+++ Lab_1/Physicist.java
@@ -1,43 +1,33 @@
-public class Physicist extends Human implements Comparable<Human>
-{
+public class Physicist extends Human implements Comparable<Human> {
     private int year;
 
     private int birthYear;
 
     private int thisYear = 2018;
 
-    public Physicist()
-    {
+    public Physicist() {
         birthYear = thisYear - getAge();
-        double randPhysicsYear = (thisYear - (birthYear + 15))*Math.random(); // Allowed range of starting years
-        int randYear = thisYear - (int)Math.floor(randPhysicsYear); // Only allowed range is selected
+        double randPhysicsYear = (thisYear - (birthYear + 15)) * Math.random(); // Allowed range of starting years
+        int randYear = thisYear - (int) Math.floor(randPhysicsYear); // Only allowed range is selected
 
-        randYear = randYear%100; // randYear is in real years, but we only want the last 2 digits
+        randYear %= 100; // randYear is in real years, but we only want the last 2 digits
 
         year = randYear;
     }
 
-    public int getYear()
-    {
+    public int getYear() {
         return year;
     }
 
-    public String toString()
-    {
-        if (year < 10) // Needed for ex. F-05, so it doesn't print F-5
-        {
-            return "Name: " + getName() + ", age: " + getAge() + ", year: F-0" + year;
-        }
-        else
-        {
-            return "Name: " + getName() + ", age: " + getAge() + ", year: F-" + year;
-        }
+    public String toString() {
+        return "Name: " + getName() + ", age: " + getAge() + ", year: F-" + String.format("%02d", year);
     }
 
-    public int compareTo(Human human)
-    {
-        if (human.getAge() > super.getAge()) return -1;
-        if (human.getAge() < super.getAge()) return 1;
+    public int compareTo(Human human) {
+        if (human.getAge() > super.getAge())
+            return -1;
+        if (human.getAge() < super.getAge())
+            return 1;
         return 0;
     }
 }

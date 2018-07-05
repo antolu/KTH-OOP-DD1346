24f2ee4ae455d63d439c655a40762d9704f58c71
Anton Lu <anton.lu@epfl.ch>
2018-07-05 16:38:39 +0200

    L4 E5.1 Export csv

--- Lab_4/Controller.java
+++ Lab_4/Controller.java
@@ -1,21 +1,23 @@
-
-
-import javax.swing.*;
+import java.io.*;
+import java.lang.StringBuilder;
 import java.awt.event.*;
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import javax.swing.event.*;
-import javax.swing.event.ChangeListener;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.*;
+import javax.swing.event.*;
 
 public class Controller extends JPanel 
                         implements ChangeListener, ActionListener {
-    Model   model;
-    View    view;
-    JSlider LSlider;
-    JSlider timeSlider;
-    Timer   timer;
-    int     dT = 1000;
+    private Model   model;
+    private View    view;
+    private JSlider LSlider;
+    private JSlider timeSlider;
+    private Timer   timer;
+    private int     dT = 1000;
+    private int     time = 0;
+    private PrintWriter outputStream = null;
+    private int     writeIndex = 0;
 
     public Controller(Model model, View view) {
         this.model = model;
@@ -43,6 +45,23 @@ public class Controller extends JPanel
         this.add(timeSlider, BorderLayout.EAST);
         this.setLayout(new FlowLayout());
 
+        try {
+            outputStream = new PrintWriter(new BufferedWriter(new FileWriter("output.csv")));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
+        /* Write lines to logging file */
+        StringBuilder outputLine = new StringBuilder(Double.toString(time));
+        for (Model.Particle particle : model.particles) {
+            outputLine.append(", ");
+            outputLine.append(particle.x);
+            outputLine.append(", ");
+            outputLine.append(particle.y);
+        }
+        outputStream.println(outputLine);
+
         timer.start();
     }
 
@@ -65,5 +84,22 @@ public class Controller extends JPanel
     public void actionPerformed(ActionEvent e) {
         model.updatePosition();
         view.repaint();
+
+        time += timer.getDelay();
+
+        if (writeIndex < 1000) {
+            StringBuilder outputLine = new StringBuilder(Integer.toString(time));
+            // String outputLine = Integer.toString(time);
+            for (Model.Particle particle : model.particles) {
+                // outputLine = outputLine + ", " + particle.x + ", " + particle.y;
+                outputLine.append(", ");
+                outputLine.append(particle.x);
+                outputLine.append(", ");
+                outputLine.append(particle.y);
+            }
+            outputStream.println(outputLine);
+
+            writeIndex++;
+        }
     }
 }

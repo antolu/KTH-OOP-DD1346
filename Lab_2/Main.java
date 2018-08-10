16955b7a531bb0cc538ca4fc6b9b188b397b737c
Anton Lu <anton.lu@epfl.ch>
2018-08-10 21:50:35 +0200

    Minor changes

--- Lab_2/Main.java
+++ Lab_2/Main.java
@@ -10,8 +10,6 @@ public class Main
     {
         JFrame frame = new JFrame("A button");
         MyButton button = new MyButton();
-        JPanel panel = new JPanel();
-
 
         frame.getContentPane().setBackground(Color.white);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Closes window when (x) is pressed in OS
@@ -19,10 +17,7 @@ public class Main
 
         // button.setPreferredSize(new Dimension(200,100));
 
-        // panel.setPreferredSize(new Dimension(300, 150));
-
-        panel.add(button);
-        frame.add(panel);
+        frame.getContentPane().add(button);
         frame.pack(); // Size frame to match contents according to layout manager
 
         frame.setVisible(true);
